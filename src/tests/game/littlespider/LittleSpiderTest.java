9116f6de4f73f4b235ba4f8fe492da95a24962db
drewf <drewf@DESKTOP-5ATFCJ8>
2018-09-23 22:27:35 -0400

    Started LittleSpiderTest

--- src/tests/game/littlespider/LittleSpiderTest.java
+++ src/tests/game/littlespider/LittleSpiderTest.java
@@ -1,5 +1,22 @@
 package tests.game.littlespider;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import code.game.littlespider.LittleSpider;
+
 public class LittleSpiderTest {
 
+	private LittleSpider game;
+	
+	@Before
+	public void newGame() {
+		LittleSpider game = new LittleSpider();
+		this.game = game;
+	}
+	
+	@Test
+	public void initTest() {
+		
+	}
 }
