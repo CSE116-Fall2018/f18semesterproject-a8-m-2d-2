88a41945bdf356097d7be02fda4090a1b01ffd44
drewf <drewf@DESKTOP-5ATFCJ8>
2018-09-24 18:04:09 -0400

    Updates test for wrap around function.

--- src/tests/game/littlespider/TableauTest.java
+++ src/tests/game/littlespider/TableauTest.java
@@ -36,15 +36,27 @@ public class TableauTest {
 		Card c = new Card(0, false);
 		Card c1 = new Card(1, false);
 		Card c2 = new Card(6, false);
+		
 		//check that cards can't be added to a empty pile
 		assertFalse("addCard returned true when adding first card", t.addCard(c1));
 		t.addFirstCard(c);
+		
+		//checks normal adds
 		assertTrue("addCard returned false when doing a legal add", t.addCard(c1));
 		assertEquals("did not update list after adding card", c1, t.getCards().get(0));
 		assertEquals("did not update top card after adding card", c1, t.getCard());
 		assertFalse("returned true on an illegal add", t.addCard(c2));
 		assertEquals("changed topCard after an illegal add", c1, t.getCard());
 		assertEquals("changed list after illegal add", c1, t.getCards().get(0));
+		
+		//test wrap around function
+		refresh();
+		t.addFirstCard(c);
+		Card c3 = new Card(12, false);
+		assertTrue("wrap around function not working properly", t.addCard(c3));
+		refresh();
+		t.addFirstCard(c3);
+		assertTrue("wrap around function not working properly", t.addCard(c));
 	}
 	
 	
