8f545c465df0589b0232112a7e3be91be7202ebe
drewf <drewf@DESKTOP-5ATFCJ8>
2018-09-24 12:40:38 -0400

    Start to initTest()

--- src/tests/game/littlespider/LittleSpiderTest.java
+++ src/tests/game/littlespider/LittleSpiderTest.java
@@ -1,13 +1,23 @@
 package tests.game.littlespider;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import code.cards.Deck;
+import code.game.littlespider.Homecell;
 import code.game.littlespider.LittleSpider;
+import code.game.littlespider.Tableau;
 
 public class LittleSpiderTest {
 
 	private LittleSpider game;
+	
+	/**
+	 * Starts a new LittleSpider instance for later testing.
+	 */
 	
 	@Before
 	public void newGame() {
@@ -15,8 +25,30 @@ public class LittleSpiderTest {
 		this.game = game;
 	}
 	
+	/**
+	 * Test LittleSpider constructor from the newGame() call.  If this test fails
+	 * all others will as well.
+	 */
+	
+	@Test
+	public void testLittleSpider() {
+		Deck deck = game.getDeck();
+		assertNotNull("No deck created", deck);
+	}
+	/**
+	 * Tests initialization of LittleSpider game.
+	 */
+	
 	@Test
 	public void initTest() {
+		Homecell[] homecells = game.getHomecells();
+		Tableau[] tableaus = game.getTableaus();
+		int homecellSize = homecells.length;
+		int tableauSize = tableaus.length;
+		
+		assertEquals("Wrong number of Tableau piles", 8, tableauSize);
+		assertEquals("Wrong number of Homecell piles", 4, homecellSize);
+		
 		
 	}
 }
