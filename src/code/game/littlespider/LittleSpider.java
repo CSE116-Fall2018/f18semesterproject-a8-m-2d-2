c99b19bf7f3ed6b53b5e4b94bd48cfbf0acbc8a0
drewf <drewf@DESKTOP-5ATFCJ8>
2018-09-24 12:40:12 -0400

    Bug fixes

--- src/code/game/littlespider/LittleSpider.java
+++ src/code/game/littlespider/LittleSpider.java
@@ -21,33 +21,35 @@ public class LittleSpider {
 	private void init() {
 		homecells = new Homecell[4];
 		
-		Card c1 = deck.getSpecificCard("ace", "diamond");
+		Card c1 = deck.getSpecificCard("diamond", "ace");
 		c1.setFaceUp();
 		Homecell homecell1 = new Homecell(c1);
 		homecells[0] = homecell1;
 		
-		Card c2 = deck.getSpecificCard("ace", "heart");
+		Card c2 = deck.getSpecificCard("heart", "ace");
 		c2.setFaceUp();
 		Homecell homecell2 = new Homecell(c2);
 		homecells[0] = homecell2;
 		
-		Card c3 = deck.getSpecificCard("king", "club");
+		Card c3 = deck.getSpecificCard("club", "king");
 		c3.setFaceUp();
 		Homecell homecell3 = new Homecell(c3);
 		homecells[0] = homecell3;
 		
-		Card c4 = deck.getSpecificCard("king", "spade");
+		Card c4 = deck.getSpecificCard("spade", "king");
 		c4.setFaceUp();
 		Homecell homecell4 = new Homecell(c4);
 		homecells[0] = homecell4;
 		
 		tableaus = new Tableau[8];
 		for(int i=0; i<tableaus.length; i++) {
+			Tableau tableau = new Tableau();
 			for(int j=1; j<=6; j++) {
 				Card x = deck.takeCard();
 				x.setFaceUp();
-				tableaus[i].addCard(x);
+				tableau.addFirstCard(x);
 			}
+			tableaus[i]= tableau;
 		}
 	}
 	public Homecell[] getHomecells() {
@@ -57,5 +59,11 @@ public class LittleSpider {
 		return tableaus;
 	}
 	
-	
+	public Deck getDeck() {
+		return deck;
+	}
+	public static void main(String[] args) {
+		LittleSpider ne = new LittleSpider();
+		System.out.println(ne.getHomecells());
+	}
 }
