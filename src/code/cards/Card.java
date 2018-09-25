e63a6a9ccd06fdb8f6ec367b0a6f69428ad9f863
thurs <thurs@LAPTOP-G1KB2F53>
2018-09-25 11:18:29 -0400

    Added javadoc comments for Card class

--- src/code/cards/Card.java
+++ src/code/cards/Card.java
@@ -1,18 +1,36 @@
 package code.cards;
 
+
+/**
+ * @author thurs
+ */
+/**
+ * @author thurs
+ *
+ */
 public class Card {
 	
+	/** The suit the card is in (e.g. heart)*/
 	private String suit;
+	/** The rank of the card (e.g. ace, 6, queen)*/
 	private String rank;
+	/** The value that the card represents*/
 	private int value;
+	/** */
 	int ID;
+	/** */
 	public boolean faceUp;
 	
+	
+	/**
+	 * @param ID - int that will determine the rank and suit of the card.
+	 * @param faceUp - boolean that determines if the card is faced up or faced down.
+	 */
 	public Card(int ID, boolean faceUp) {
 		this.ID = ID;
 		int suit = ID/13;
 		Integer rank = (int) ID%13;
-		this.setValue(rank);
+		this.value = rank;
 		switch(suit) {
 		case 0:
 			this.suit = "diamond";
@@ -28,6 +46,7 @@ public class Card {
 			break;
 		default:
 			System.out.println("no matching suit");
+			
 		}
 		
 		if (rank == 0)
@@ -45,28 +64,37 @@ public class Card {
 		this.faceUp = faceUp;
 	}
 
+	/**
+	 * Sets the field faceUp as true.
+	 */
 	public void setFaceUp() {
 		this.faceUp = true;
 	}
 
+	/**
+	 * @return the instance String referencing the rank of the card.
+	 */
 	public String getRank() {
 		return this.rank;
 	}
 	
+	/**
+	 * @return the instance String referencing the suit of the card.
+	 */
 	public String getSuit() {
 		return this.suit;
 	}
 
-	public String toString() {
-		return rank +" of " +suit;
-	}
-
+	/**
+	 * @return the instance int referencing the value of the card.
+	 */
 	public int getValue() {
 		return value;
 	}
-
-	public void setValue(int value) {
-		this.value = value;
+	
+	@Override
+	public String toString() {
+		return rank +" of " +suit;
 	}
 	
 }
