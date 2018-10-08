83aefda09a868c8c6c96a07a8c87f40f8389002e
drewf <drewf@DESKTOP-5ATFCJ8>
2018-10-08 14:53:34 -0400

    menu bar looks better

--- src/code/game/GUI/GUI.java
+++ src/code/game/GUI/GUI.java
@@ -1,8 +1,11 @@
 package code.game.GUI;
 
+import java.awt.Font;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.UIManager;
 
 import code.game.GUI.control.ExitListener;
 import code.game.GUI.control.GolfListener;
@@ -11,19 +14,27 @@ import code.game.GUI.control.LittleSpiderListener;
 public class GUI {
 
 	public JMenuBar getMenuBar() {
+		Font font = new Font("Arial", Font.PLAIN, 35);
+        UIManager.put("Menu.font", font);
+        UIManager.put("MenuItem.font", font);
+		
 		JMenuBar menuBar = new JMenuBar();
 		JMenu menu = new JMenu("New Game");
+		menuBar.add(menu);
+		
 		JMenuItem littleSpider = new JMenuItem("Little Spider");
 		littleSpider.addActionListener(new LittleSpiderListener());
 		menu.add(littleSpider);
+		
 		JMenuItem golf = new JMenuItem("Golf");
 		golf.addActionListener(new GolfListener());
 		menu.add(golf);
-		menuBar.add(menu);
+
 		JMenuItem exit = new JMenuItem("Exit");
 		exit.addActionListener(new ExitListener());  
 		menu.add(exit);
+		
 		return menuBar;
 	}
-	
+
 }
