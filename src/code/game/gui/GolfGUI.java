0ba55ae5549434237b00ca1c2bb2ee892bb5753e
dylanschillaci <dylanschillaci@10.84.94.179>
2018-10-12 16:11:11 -0400

    Created base for Golf Game GUI. And removed GUI Card class.

--- src/code/game/gui/GolfGUI.java
+++ src/code/game/gui/GolfGUI.java
@@ -1,5 +1,43 @@
 package code.game.gui;
 
-public class GolfGUI {
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+import code.cards.Card;
+import code.cards.Pile;
+import code.game.golf.*;
+
+public class GolfGUI {
+	JLabel golf;
+	Golf game;
+	JLabel[] tabs;
+	JLabel homecell;
+	JLabel stockpile;
+	
+	public GolfGUI() {
+		this.golf = new JLabel();
+		this.game = new Golf();
+		
+	}
+	
+	public void refresh() {
+		Tableau[] tabArr = this.game.getTableaus();
+		this.homecell = this.game.getHomecell().getCard().getIcon();
+		this.stockpile = this.game.getStockpile().getCard().getIcon();
+		for(int i = 0; i < tabArr.length; i++) {
+			Card c = tabArr[i].getCard();
+			tabs[i] =  c.getIcon();
+		}		
+	}
+	
+	
+	public JPanel getGame() {
+		JPanel panel = new JPanel();
+		
+		
+		
+		
+		return panel;
+	}
+	
 }
