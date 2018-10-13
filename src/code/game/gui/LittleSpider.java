3d1c1bb47dcdb46629f371b461c77b84f2015719
drewf <drewf@DESKTOP-5ATFCJ8>
2018-10-12 22:33:31 -0400

    New gui.LittleSpider

--- src/code/game/gui/LittleSpider.java
+++ src/code/game/gui/LittleSpider.java
@@ -1,19 +1,33 @@
 package code.game.gui;
 
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-//import com.sun.prism.Image;
 
+import code.game.littlespider.Tableau;
+import code.game.gui.control.ClickListener;
 import code.game.littlespider.Homecell;
 
 public class LittleSpider {
+	
+	private JLabel[] homecellLabels;
+	private JLabel[] tableauLabels;
+	private code.game.littlespider.LittleSpider game;
+	private Homecell[] homecells;
+	private Tableau[] tableaus;
+	
+	public LittleSpider() {
+		homecellLabels = new JLabel[4];
+		tableauLabels = new JLabel[8];
+		game = new code.game.littlespider.LittleSpider();
+		homecells = game.getHomecells();
+		tableaus = game.getTableaus();
+	}
 
 	public JPanel getGame() {
 		JPanel panel = new JPanel();
@@ -21,71 +35,75 @@ public class LittleSpider {
 		panel.setLayout(layout);
 		GridBagConstraints c = new GridBagConstraints();
 		
-		code.game.littlespider.LittleSpider game = new code.game.littlespider.LittleSpider();
-		Homecell[] homecells = game.getHomecells();
-		
-		// get card info
-		
-		JLabel title = new JLabel("Little Spider");
+		JLabel title = new JLabel(" Little Spider");
 		title.setFont(new Font("Arial", Font.BOLD, 75));
 		c.gridx = 0;
 		c.gridy = 0;
 		c.gridwidth = 4;
 		c.fill = GridBagConstraints.HORIZONTAL;
+		c.insets = new Insets(20,20,20,20);
 		panel.add(title, c);
 		
-		JLabel h1 = getImageLabel("/ad.png");
+		JLabel home = new JLabel("   Homecells:");
+		home.setFont(new Font("Arial", Font.PLAIN, 60));
 		c.gridx = 0;
 		c.gridy = 1;
-		c.gridwidth = 1;
-		c.fill = GridBagConstraints.NONE;
-		panel.add(h1, c);
+		c.gridwidth = 4;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		panel.add(home, c);
 		
-		JLabel h2 = getImageLabel("/ah.png");
-		c.gridx = 1;
-		c.gridy = 1;
-		panel.add(h2, c);
-		
-		JLabel h3 = getImageLabel("/kc.png");
-		c.gridx = 2;
-		c.gridy = 1;
-		panel.add(h3, c);
-		
-		JLabel h4 = getImageLabel("/ks.png");
-		c.gridx = 3;
-		c.gridy = 1;
-		panel.add(h4, c);
-		
-		
-		JLabel retVal = getImageLabel("/10h.png");
+		JLabel tab = new JLabel("   Tableaus:");
+		tab.setFont(new Font("Arial", Font.PLAIN, 60));
 		c.gridx = 0;
-		c.gridy = 2;
-		panel.add(retVal, c);
+		c.gridy = 3;
+		c.gridwidth = 4;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		panel.add(tab, c);
 		
+		int x = 0;
+		for(int i=0; i<homecells.length; i++) {
+			Homecell h = homecells[i];
+			code.cards.Card cc = h.getCard();
+			JLabel lab = cc.getIcon();
+			c.gridx = x;
+			c.gridy = 2;
+			c.gridwidth = 1;
+			c.fill = GridBagConstraints.NONE;
+			lab.addMouseListener(new ClickListener());
+			panel.add(lab, c);
+			homecellLabels[i] = lab;;
+			x=x+1;
+		}
+		
+		x = 0;
+		int y = 4;
+		for(int i=0; i<tableaus.length; i++) {
+			Tableau h = tableaus[i];
+			code.cards.Card cc = h.getCard();
+			JLabel lab = cc.getIcon();
+			c.gridx = x;
+			c.gridy = y;
+			c.gridwidth = 1;
+			c.fill = GridBagConstraints.NONE;
+			lab.addMouseListener(new ClickListener());
+			panel.add(lab, c);
+			x=x+1;
+			tableauLabels[i] = lab;
+			if(x==4) {
+				x=0;
+				y=5;
+			}
+		}
+	
 		JLabel output = new JLabel("");
 		panel.add(output);
 		output.setFont(new Font("Arial", Font.PLAIN, 35));
-		
+		panel.setBackground(GUI.BG_COLOR);
 		return panel;
 	}
 	
-	public JLabel getImageLabel(String file) {
-		JLabel retVal = new JLabel();
-	    java.net.URL imgURL = this.getClass().getResource(file); // file is in form "/filename.git" with new source folder configuration
-	    if (imgURL == null) {
-	      throw new IllegalArgumentException("Couldn't find file: " + imgURL);
-	    }
-	    ImageIcon cardImage = new ImageIcon(imgURL);
-	    java.awt.Image image = cardImage.getImage();
-	    java.awt.Image newimg = image.getScaledInstance(219, 291,  java.awt.Image.SCALE_SMOOTH); // 73,97 (original dimensions) resizes card by a 
-	    cardImage = new ImageIcon(newimg);  													// factor of 3 so that it is not tiny
-	    
-	    retVal.setIcon(cardImage);
-	    Dimension d = new Dimension(cardImage.getIconWidth() + 20, cardImage.getIconHeight() + 20); // creates a small separation between cards
-	    retVal.setSize(d);
-	    retVal.setPreferredSize(d);
-	    retVal.setMaximumSize(d);
-	    retVal.setMinimumSize(d);
-		return retVal;
+	
+	public void addSelect() {
+		
 	}
 }
