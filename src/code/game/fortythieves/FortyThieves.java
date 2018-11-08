e6116247b264894643993430893263578f244ec7
mferrera <matt.ferrera@gmail.com>
2018-11-08 10:53:54 -0500

    Properly create homecells, stockpiles, tableaus

--- src/code/game/fortythieves/FortyThieves.java
+++ src/code/game/fortythieves/FortyThieves.java
@@ -8,6 +8,8 @@ import javax.swing.JLabel;
 import code.cards.Card;
 import code.cards.Deck;
 import code.game.Game;
+import code.game.fortythieves.Homecell;
+import code.game.fortythieves.Stockpile;
 import code.game.gui.GUI;
 import code.game.gui.control.EndGame;
 
@@ -22,6 +24,10 @@ public class FortyThieves extends Game {
 	private static final long serialVersionUID = 1L;
 	/** Holds an array of Homecell instances that are used by the game. */
 	private Homecell[] homecells;
+	/** Homecell is the Homecell object where cards will be placed throughout the game. */
+	private Wastepile wastepile;
+	/** Stockpile is the game's Stockpile object. */
+	private Stockpile stockpile;
 	/** The amount of vertical offset per card in same pile. */
 	private static final int Y_OFFSET = 25;
 	/** The horizontal offset to the right of each tableau. */
@@ -40,47 +46,82 @@ public class FortyThieves extends Game {
 	 * Creates Homecell and Tableau piles for Little Spider.
 	 */
 	protected void init() {
-		Deck deck = new Deck(this);
-		deck.shuffle();
+		homecells = new Homecell[8];
+		wastepile = new Wastepile(this);
 		this.setTableauSelected(null);
 		
-		homecells = new Homecell[4];
-		
+		ArrayList<Card> allCards = new ArrayList<>();
+	
+		Deck deck = new Deck(this);
+		deck.shuffle();
+
 		Card c1 = deck.getSpecificCard("diamond", "ace");
 		c1.flip();
 		Homecell homecell = new Homecell(c1, this);
 		homecells[0] = homecell;
 		homecell.setHomecellNum(0);
-		
+
 		Card c2 = deck.getSpecificCard("heart", "ace");
 		c2.flip();
 		Homecell homecell2 = new Homecell(c2, this);
 		homecells[1] = homecell2;
 		homecell2.setHomecellNum(1);
-		
-		Card c3 = deck.getSpecificCard("club", "king");
+
+		Card c3 = deck.getSpecificCard("club", "ace");
 		c3.flip();
 		Homecell homecell3 = new Homecell(c3, this);
 		homecells[2] = homecell3;
 		homecell3.setHomecellNum(2);
-		
-		Card c4 = deck.getSpecificCard("spade", "king");
+
+		Card c4 = deck.getSpecificCard("spade", "ace");
 		c4.flip();
 		Homecell homecell4 = new Homecell(c4, this);
 		homecells[3] = homecell4;
 		homecell4.setHomecellNum(3);
 		
-		tableaus = new Tableau[8];
+		allCards.addAll(deck.getAllCards());
+		deck = new Deck(this);
+		deck.shuffle();
+		
+		Card c5 = deck.getSpecificCard("diamond", "ace");
+		c5.flip();
+		Homecell homecell5 = new Homecell(c5, this);
+		homecells[4] = homecell5;
+		homecell.setHomecellNum(4);
+
+		Card c6 = deck.getSpecificCard("heart", "ace");
+		c6.flip();
+		Homecell homecell6 = new Homecell(c6, this);
+		homecells[5] = homecell6;
+		homecell2.setHomecellNum(5);
+
+		Card c7 = deck.getSpecificCard("club", "ace");
+		c7.flip();
+		Homecell homecell7 = new Homecell(c7, this);
+		homecells[6] = homecell7;
+		homecell3.setHomecellNum(6);
+
+		Card c8 = deck.getSpecificCard("spade", "ace");
+		c8.flip();
+		Homecell homecell8 = new Homecell(c8, this);
+		homecells[7] = homecell8;
+		homecell4.setHomecellNum(7);
+		
+		allCards.addAll(deck.getAllCards());
+		
+		tableaus = new Tableau[13];
 		for(int i=0; i<tableaus.length; i++) {
 			Tableau tableau = new Tableau();
-			for(int j=1; j<=6; j++) {
-				Card x = deck.takeCard();
+			for(int j=1; j<=3; j++) {
+				Card x = allCards.remove(0);
 				x.flip();
 				x.setTableauNum(i);
 				tableau.addCard(x, true);
 			}
 			tableaus[i]= tableau;
 		}
+		
+		this.stockpile = new Stockpile(this, allCards);
 		refresh();
 		setMoves(0);
 	}
